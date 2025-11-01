0c14b3a273bef714a7092bc696cda243f0744db0
24mca019 <24mca019@nirmauni.ac.in>
2025-11-01 17:18:43 +0530

    Fix Vercel app handler and staticfiles warning

--- vinyasa_project/wsgi.py
+++ vinyasa_project/wsgi.py
@@ -1,11 +1,9 @@
-"""
-WSGI config for vinyasa_project project.
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vinyasa_project.settings')
 
 application = get_wsgi_application()
+
+# ✅ Add this line for Vercel
+app = application  # or handler = application (both work)

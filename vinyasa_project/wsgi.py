ea49c74d2d46b3dcb3ef0038742bfffe04d7a84f
24mca019 <24mca019@nirmauni.ac.in>
2025-11-01 17:25:01 +0530

    Fix vercel.json and wsgi handler

--- vinyasa_project/wsgi.py
+++ vinyasa_project/wsgi.py
@@ -5,6 +5,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vinyasa_project.settings')
 
 application = get_wsgi_application()
 
-# ✅ Required for Vercel (must be one of these)
-app = application  # Vercel looks for this
-handler = application  # (backup, just in case)
+# Required for Vercel
+app = application
+handler = application
